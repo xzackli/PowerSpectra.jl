6b71fec198eb4267af1eb9adb596a2ed47fde40c
xzackli <xzackli@gmail.com>
2020-06-11 17:31:50 -0400

    new correlated noise simulations

--- examples/noise.jl
+++ examples/noise.jl
@@ -3,8 +3,67 @@ using Healpix
 using PyCall, PyPlot
 using CSV, DataFrames, LinearAlgebra
 using BenchmarkTools
+using Distributions, Random
+import ThreadPools: @qthreads
+using BenchmarkTools
 hp = pyimport("healpy")
 nmt = pyimport("pymaster")
+
+##
+
+nside = 256
+noise = CSV.read("notebooks/data/noise.csv")
+nltt = convert(Vector{Float64}, noise.nltt)
+nalm0 = AngularPowerSpectra.generate_correlated_noise(nside, 10 * 2π / 180, nltt)
+
+pow = alm2cl((nalm))
+x = Float64.(collect(eachindex(pow)))
+y = log.(pow)
+
+clf()
+plot(pow)
+n0 = 1.5394030890788515 / nside
+plot(nltt)
+yscale("log")
+gcf()
+
+##
+
+function generate_mean_noise_spectrum(nside, nltt)
+    n = AngularPowerSpectra.generate_correlated_noise(nside, 10 * 2π / 180, nltt)
+    pow = alm2cl(map2alm(n))
+    for i ∈ 1:3
+        n = AngularPowerSpectra.generate_correlated_noise(nside, 10 * 2π / 180, nltt)
+        pow .+= alm2cl(map2alm(n))
+    end
+    pow ./= 4.0
+    pow
+end
+
+pow = generate_mean_noise_spectrum(nside, nltt)
+##
+
+x = Float64.(collect(eachindex(pow)))
+y = log.(pow)
+
+clf()
+plot(pow)
+n0 = 1.5394030890788515 / nside
+plot(n0 ./ x)
+plot(nltt)
+# axhline(n0)
+# plot(ss.savgol_filter(ys, 51, 1))
+yscale("log")
+gcf()
+
+##
+
+
+##
+clf()
+hp.mollview(, 
+            title=raw"correlations in $\phi$")
+gcf()
 
 ##
 nside = 256
@@ -28,19 +87,23 @@ workspace = SpectralWorkspace(m_143_hm1, m_143_hm2, m_143_hm1, m_143_hm2)
 @time factorized_mcm = cholesky(Hermitian(mcm.parent));
 
 ##
-function get_sim()
+function get_sim(nside)
     pixwin = true
     signal = hp.synfast(theory.cltt, 
         nside, verbose=false, pixwin=pixwin, new=true)
-    n1 = hp.synfast(noise.nltt, 
-        nside, verbose=false, pixwin=pixwin, new=true)
-    n2 = hp.synfast(noise.nltt, 
-        nside, verbose=false, pixwin=pixwin, new=true)
+    n1 = AngularPowerSpectra.generate_correlated_map(nside, 10 * 2π / 180)
+    n2 = AngularPowerSpectra.generate_correlated_map(nside, 10 * 2π / 180)
+    # n1 = hp.synfast(noise.nltt, 
+    #     nside, verbose=false, pixwin=pixwin, new=true)
+    # n2 = hp.synfast(noise.nltt, 
+    #     nside, verbose=false, pixwin=pixwin, new=true)
     map1 = Map{Float64, RingOrder}(nside)
     map1.pixels .= hp.sphtfunc.smoothing(signal .+ n1, beam_window=Bl1, verbose=false)
+    map1.pixels .*= mask1.pixels  # pseudo-Cl
     map2 = Map{Float64, RingOrder}(nside)
     map2.pixels .= hp.sphtfunc.smoothing(signal .+ n2, beam_window=Bl2, verbose=false)
-    Clhat = compute_spectra(map1 * mask1, map2 * mask2, factorized_mcm, beam1, beam2)
+    map2.pixels .*= mask2.pixels  # pseudo-Cl
+    Clhat = compute_spectra(map1, map2, factorized_mcm, beam1, beam2)
     return Clhat
 end
 
@@ -48,18 +111,17 @@ function generate_sim_array(nsims)
 
     result = Array{Float64, 2}(undef, (3 * nside, nsims))
     for i in 1:nsims
-        Clhat = get_sim()
+        Clhat = get_sim(nside)
         result[:,i] .= Clhat
     end
 
     return result
 end
 
-using JLD2
-# sims = generate_sim_array(1000)
-# @save "sims.jld2" sims
-@load "sims.jld2" sims
+sims = generate_sim_array(1000)
 
+using JLD2
+@save "sims.jld2" sims
 
 ##
 
--- src/AngularPowerSpectra.jl
+++ src/AngularPowerSpectra.jl
@@ -5,17 +5,21 @@ import Base.Threads: @threads
 import UnsafeArrays: uview, UnsafeArray
 import DataStructures: DefaultDict
 import Combinatorics: permutations, combinations, with_replacement_combinations
-import Healpix: Map, PolarizedMap, Alm, RingOrder, alm2cl, map2alm, numberOfAlms
+import Healpix: Map, PolarizedMap, Alm, RingOrder, alm2cl, map2alm, numberOfAlms,
+    RingInfo, getringinfo!, almIndex, alm2map
 import WignerFamilies: wigner3j_f!, WignerF, WignerSymbolVector, get_wigner_array, 
     swap_triangular
 import FillArrays: Zeros
 import OffsetArrays: OffsetArray, OffsetVector
 import LinearAlgebra: Cholesky, ldiv!, rdiv!, Hermitian
-import LoopVectorization: @avx
+import Distributions: MvNormal
+using Random
+# import LoopVectorization: @avx
 
 export compute_mcm, compute_spectra, compute_covmat
 export Field, SpectralWorkspace, SpectralVector, SpectralArray
 
+include("util.jl")
 include("spectralarray.jl")
 include("workspace.jl")
 include("modecoupling.jl")
