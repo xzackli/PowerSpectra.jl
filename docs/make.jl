db11fef8ecaf8f8a6a70853a511d0028fa401f2b
xzackli <xzackli@gmail.com>
2020-12-23 20:30:34 -0500

    remake CI

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Documenter
 
 makedocs(;
     modules=[AngularPowerSpectra],
-    authors="xzackli <xzackli@gmail.com> and contributors",
+    authors="Zack Li",
     repo="https://github.com/xzackli/AngularPowerSpectra.jl/blob/{commit}{path}#L{line}",
     sitename="AngularPowerSpectra.jl",
     format=Documenter.HTML(;
