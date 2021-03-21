876a82453549092a01793f5d1a533e27328d16e7
xzackli <xzackli@gmail.com>
2021-03-21 14:41:22 -0400

    optimized quickpol Xi

--- docs/src/beams.md
+++ docs/src/beams.md
@@ -4,12 +4,15 @@ CurrentModule = AngularPowerSpectra
 
 # QuickPol
 
-We provide utilities to compute beam matrices in the QuickPol formalism ([Hivon et al. 2017](https://arxiv.org/abs/1608.08833)). We introduce some extra notation here for convenience. Define the matrix,
+We provide utilities to compute beam matrices in the QuickPol formalism ([Hivon et al. 2017](https://arxiv.org/abs/1608.08833)). We introduce some additional steps here for computational efficiency. In this section, we use the indices ``\ell, \ell', \ell''`` such that we don't need to change indices at the end in order to match Hivon. Define a scaled version of the scan spectrum
+```math
+W_{\ell'}^{\nu_1,\nu_2,s_1,s_2,j_1,j_2} = \sum_{m^\prime=-\ell^\prime}^{\ell^\prime}  \left(_{s_1+\nu_1}\tilde{\omega}^{(j_1)}_{\ell^\prime m^\prime}\right)
+    \left(_{s_2+\nu_2}\tilde{\omega}^{(j_2)}_{\ell^\prime m^\prime}\right)^*
+```
+Define the matrix,
 ```math
 \begin{aligned}
-\mathbf{\Xi}^{\nu_1,\nu_2,s_1,s_2,j_1,j_2}_{\ell^{\prime\prime},\ell} &= (-1)^{s_1 + s_2 + \nu_1 + \nu_2} \sum_{\ell^{\prime} m^{\prime}} \, \rho_{j_1,\nu_1} \rho_{j_2, \nu_2} \times \\
-&\qquad \left(_{s_1+\nu_1}\tilde{\omega}^{(j_1)}_{\ell^\prime m^\prime}\right)
-    \left(_{s_2+\nu_2}\tilde{\omega}^{(j_2)}_{\ell^\prime m^\prime}\right)^*  \\
+\mathbf{\Xi}^{\nu_1,\nu_2,s_1,s_2,j_1,j_2}_{\ell^{\prime\prime},\ell} &= (-1)^{s_1 + s_2 + \nu_1 + \nu_2} \sum_{\ell^{\prime}} \, \rho_{j_1,\nu_1} \rho_{j_2, \nu_2}  W_{\ell'}^{\nu_1,\nu_2,s_1,s_2,j_1,j_2}  \\
  &\qquad\qquad \times \begin{pmatrix} \ell & \ell^{\prime} & \ell^{\prime\prime} \\
      -s_1 & s_1+\nu_1  & -\nu_1 \end{pmatrix} \begin{pmatrix}
      \ell & \ell^{\prime} & \ell^{\prime\prime} \\ -s_2 & s_2+\nu_2  & -\nu_2 \end{pmatrix}
@@ -29,4 +32,6 @@ Note that the inner sum is just a matrix-vector multiplication.
 
 ```@docs
 quickpolΞ!
+quickpolW
+Ξsum
 ``` 
--- src/beam.jl
+++ src/beam.jl
@@ -1,46 +1,4 @@
 
-
-# @doc raw"""
-#     clquickpol(nu₁, nu₂, b₁, b₂, ρ₁, ρ₂, ω₁, ω₂)
-
-# Compute the cross-power spectrum of two maps with spins ``\nu_1`` and ``\nu_2``
-# (Hivon et al. 2016, eq. 38)
-
-# ```math
-# \begin{aligned}
-# \tilde{C}_{\ell^{\prime\prime}}^{\nu_1 \nu_2}  &= \sum_{u_1, u_2, j_1, j_2, \ell, s_1, s_2}
-#     (-1)^{s_1 + s_1 + \nu_1 + \nu_2} C_{\ell}^{u_1 u_2} \frac{2\ell+1}{4\pi}
-#     \,_{u_1}\hat{b}^{(j_1)*}_{\ell s_1} \,_{u_2}\hat{b}^{(j_2)*}_{\ell s_2} \\
-# &\qquad\qquad \times \frac{k_{u_1} k_{u_2}}{k_{\nu_1}k_{\nu_2}} \sum_{\ell^\prime m^\prime}
-#     \rho_{j_1, \nu_1} \rho_{j_2, \nu_2} (_{s_1+\nu_1}\tilde{\omega}^{(j_1)}_{\ell^\prime m^\prime})
-#     (_{s_2+\nu_2}\tilde{\omega}^{(j_2)}_{\ell^\prime m^\prime})^* \\
-# &\qquad\qquad \times \begin{pmatrix} \ell & \ell^{\prime} & \ell^{\prime\prime} \\
-#     -s_1 & s_1+\nu_1  & -\nu_1 \end{pmatrix} \begin{pmatrix}
-#     \ell & \ell^{\prime} & \ell^{\prime\prime} \\ -s_2 & s_2+\nu_2  & -\nu_2 \end{pmatrix}
-# \end{aligned}
-# ```
-
-# # Arguments:
-# - `nu₁`: spin of the first map
-# - `nu₂`: spin of the second map
-# - `b₁::SpectralVector`: inverse noise-weighted beam multipoles for the first map
-# - `b₂::SpectralVector`: inverse noise-weighted beam multipoles for the second map
-# - `ρ₁`: polarization efficiency
-# - `ρ₂`: polarization efficiency
-# - `ω₁`: effective weights describing the scanning of the first map
-# - `ω₂`: effective weights describing the scanning of the second map
-
-
-# # Returns:
-# - `SpectralVector`: The cross-power spectrum of the two provided maps.
-
-# """
-# function clquickpol(nu₁, nu₂, b₁::SV, b₂::SV, ρ₁, ρ₂, ω₁, ω₂) where {T, SV <: SpectralVector{T}}
-
-# end
-
-
-
 @doc raw"""
     Ξsum(alm₁, alm₂, w3j₁, w3j₂)
 
@@ -50,38 +8,52 @@ in this summation, as they can be pulled out.
 
 ```math
 \begin{aligned}
-(\Xi \mathrm{sum}) &= \sum_{\ell^{\prime} m^{\prime}} \,  \left(_{s_1+\nu_1}\tilde{\omega}^{(j_1)}_{\ell^\prime m^\prime}\right)
-    \left(_{s_2+\nu_2}\tilde{\omega}^{(j_2)}_{\ell^\prime m^\prime}\right)^*  \\
- &\qquad\qquad \times \begin{pmatrix} \ell & \ell^{\prime} & \ell^{\prime\prime} \\
+(\Xi \mathrm{sum}) &= \sum_{\ell^{\prime} m^{\prime}} \,  W_{\ell'}^{\nu_1,\nu_2,s_1,s_2,j_1,j_2} \times \begin{pmatrix} \ell & \ell^{\prime} & \ell^{\prime\prime} \\
      -s_1 & s_1+\nu_1  & -\nu_1 \end{pmatrix} \begin{pmatrix}
      \ell & \ell^{\prime} & \ell^{\prime\prime} \\ -s_2 & s_2+\nu_2  & -\nu_2 \end{pmatrix}
 \end{aligned}
 ```
 """
-function Ξsum(alm₁::Alm{Complex{T}}, alm₂::Alm{Complex{T}},
+function Ξsum(W::SpectralVector{T},
               w3j₁::WignerSymbolVector{T, Int},
               w3j₂::WignerSymbolVector{T, Int}) where {T<:Number}
 
-    mmax = min(alm₁.mmax, alm₂.mmax)
     ℓ_start = max(firstindex(w3j₁), firstindex(w3j₂))
     ℓ_end = min(lastindex(w3j₁), lastindex(w3j₂))
     Σ = zero(T)
-    for ℓ = ℓ_start:ℓ_end  # sum over nontrivial Wigner 3j symbols
-        ℓ_term = zero(T)  # term in the summation over ℓ
-        for m = 1:min(ℓ, mmax)  # loop over -m:m (symmetric, but skip m=0)
-            index = almIndex(alm₁, ℓ, m)
-            ℓ_term += 2 * real(alm₁.alm[index] * conj(alm₂.alm[index]))
-        end
-        index0 = almIndex(alm₁, ℓ, 0)  # now add in the m=0 term
-        ℓ_term += real(alm₁.alm[index0] * conj(alm₂.alm[index0]))
-        Σ += ℓ_term * w3j₁[ℓ] * w3j₂[ℓ]  # accumulate in ℓ. we pull 3j out of m sum
+    @inbounds @simd for ℓ = ℓ_start:ℓ_end  # sum over nontrivial Wigner 3j symbols
+        Σ += W[ℓ] * w3j₁[ℓ] * w3j₂[ℓ]  # accumulate in ℓ. we pull 3j out of m sum
     end
     return Σ
 end
 
+@doc raw"""
+    quickpolW(alm₁::Alm{Complex{T}}, alm₂::Alm{Complex{T}})
+
+Computes a scaled spectrum of the scanned pattern.
+
+```math
+W_{\ell'}^{\nu_1,\nu_2,s_1,s_2,j_1,j_2} = \sum_{m^\prime=-\ell^\prime}^{\ell^\prime}  \left(_{s_1+\nu_1}\tilde{\omega}^{(j_1)}_{\ell^\prime m^\prime}\right)
+    \left(_{s_2+\nu_2}\tilde{\omega}^{(j_2)}_{\ell^\prime m^\prime}\right)^*
+```
+"""
+function quickpolW(alm₁::Alm{Complex{T}}, alm₂::Alm{Complex{T}}) where T<:Number
+    mmax = min(alm₁.mmax, alm₂.mmax)
+    lmax = min(alm₁.lmax, alm₂.lmax)
+    cl = SpectralVector(zeros(T, lmax + 1))
+    for ℓ = 0:lmax  # fill in cl for each ℓ
+        for m = 1:min(ℓ, mmax)  # loop over m (symmetric, but skip m=0)
+            index = almIndex(alm₁, ℓ, m)
+            cl[ℓ] += 2 * real(alm₁.alm[index] * conj(alm₂.alm[index]))
+        end
+        index0 = almIndex(alm₁, ℓ, 0)  # now do m=0
+        cl[ℓ] += real(alm₁.alm[index0] * conj(alm₂.alm[index0]))
+    end
+    return cl
+end
 
 @doc raw"""
-    quickpolΞ!(𝚵::AA, ν₁, ν₂, u₁, u₂, s₁, s₂, ω₁, ω₂, b₁, b₂, buf1, buf2)
+    quickpolΞ!(𝚵::AA, ν₁, ν₂, u₁, u₂, s₁, s₂, ω₁, ω₂, b₁, b₂)
 
 This computes the ``\Xi_{\ell^{\prime \prime},\ell}`` matrix. It assumes ``\rho`` has been
 absorbed into the ``\omega`` terms.
@@ -97,6 +69,7 @@ function quickpolΞ!(𝚵::AA, ν₁, ν₂, s₁, s₂, ω₁::Alm, ω₂::Alm,
     # make some basic checks
     size(𝚵,1) != size(𝚵,2) && throw(ArgumentError("𝚵 is not square."))
     lmax = lastindex(𝚵,1)  # indexed 0:lmax
+    W = quickpolW(ω₁, ω₂)
 
     @qthreads for ℓ″ = 2:lmax
         tid = Threads.threadid()
@@ -112,7 +85,7 @@ function quickpolΞ!(𝚵::AA, ν₁, ν₂, s₁, s₂, ω₁::Alm, ω₂::Alm,
             w3j₂ = WignerSymbolVector(bufferview2, wF₂.nₘᵢₙ:wF₂.nₘₐₓ)
             wigner3j_f!(wF₁, w3j₁)  # deposit symbols into buffer
             wigner3j_f!(wF₂, w3j₂)  # deposit symbols into buffer
-            𝚵[ℓ″, ℓ] = Ξsum(ω₁, ω₂, w3j₁, w3j₂)
+            𝚵[ℓ″, ℓ] = Ξsum(W, w3j₁, w3j₂)
             𝚵[ℓ, ℓ″] = 𝚵[ℓ″, ℓ]
         end
     end
